69d39f8d58a21aeb3218865d0c720776f9ef704b
caffe <984770193@qq.com>
2019-08-01 20:59:35 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/CompensateScatter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompensateScatter.dir/main.cpp.o"
+  "CompensateScatter.pdb"
+  "CompensateScatter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompensateScatter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/scatter.h
@@ -0,0 +1,13 @@
+#ifndef COMPENSATESCATTER_SCATTER_H
+#define COMPENSATESCATTER_SCATTER_H
+/**
+ * This class is an implement version of paper "Compensating Indirect Scattering for immersive and Semi-Immersive
+ * Projection Display", which is used to compensate the projector-camera system in L-shape screen.This method is a
+ * tradition method for photometric compensation and need to understand the geometric information of projection
+ * environment,and using iterator method to compensate images eventually.The detail inner-principle of this
+ * program you'd better to refer the paper,which I have provided in the "resource/paper" directory.
+ */
+class Scatter{
+
+};
+#endif //COMPENSATESCATTER_SCATTER_H
